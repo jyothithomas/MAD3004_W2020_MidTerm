1ba9bfbe63c01df82761b78ce4ec992519438d0c
Jyothi Thomas <jyothithomas.05@gmail.com>
2020-02-13 16:59:45 -0500

    [jyothi] commit

---
+++ CO775696_W2020_MID_TERM_MAD3004 a/CO775696_W2020_MID_TERM_MAD3004 a/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CO775696_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 Lambton College. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
