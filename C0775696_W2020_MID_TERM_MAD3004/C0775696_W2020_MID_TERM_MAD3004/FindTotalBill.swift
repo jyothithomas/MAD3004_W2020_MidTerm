92d0f1914b53a13df89cdf5431d3e153a082b07a
Avani Patel <anpatel36@yahoo.in>
2020-02-17 15:38:17 -0500

    [jyothi] commit

--- C0775696_W2020_MID_TERM_MAD3004/C0775696_W2020_MID_TERM_MAD3004/FindTotalBill.swift
+++ C0775696_W2020_MID_TERM_MAD3004/C0775696_W2020_MID_TERM_MAD3004/FindTotalBill.swift
@@ -2,10 +2,9 @@
 //  FindTotalBill.swift
 //  C0775696_W2020_MID_TERM_MAD3004
 //
-//  Created by MacStudent on 2020-02-17.
+//  Created by Jyothi Thomas on 2/17/20.
 //  Copyright © 2020 Lambton College. All rights reserved.
 //
-
 
 import Foundation
 
